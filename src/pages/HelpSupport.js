1c55c70e4126e26382441c21ce3f335d168fa525
harshitsaini <harshitsaini15@gmail.com>
2023-03-04 11:53:19 +0530

    Dissolved Button2

--- src/pages/HelpSupport.js
+++ src/pages/HelpSupport.js
@@ -1,5 +1,4 @@
 import Navbar2 from "../components/Navbar2";
-import Button2 from "../components/Button2";
 import Button from "../components/Button";
 import "./HelpSupport.css";
 
@@ -8,37 +7,22 @@ const HelpSupport = () => {
     <div className="helpsupport">
       <Navbar2 />
       <div className="apphomeinner">
-        <Button2 />
+        <Button
+          label="Request a feature"
+          buttonType="light"
+          iconXSmall="../iconxsmall21@2x.png"
+          iconXSmallDisplay="unset"
+        />
         <Button
           label="Submit a bug"
-          buttonDerivativeBaseBoxShadow="0px 0px 4px rgba(0, 0, 0, 0.25)"
-          buttonDerivativeBaseOverflow="unset"
-          buttonDerivativeBaseJustifyContent="center"
-          buttonBackgroundColor="#fff"
-          labelLineHeight="125%"
-          labelColor="#000"
-          iconXSmall="../iconxsmall20@2x.png"
-          buttonDerivativeBasePosition="absolute"
-          buttonDerivativeBaseWidth="8.83%"
-          buttonDerivativeBaseTop="calc(50% - 290px)"
-          buttonDerivativeBaseRight="43.17%"
-          buttonDerivativeBaseLeft="48.01%"
+          buttonType="light"
+          iconXSmall="../iconxsmall21@2x.png"
           iconXSmallDisplay="unset"
         />
         <Button
           label="Contact customer care"
-          buttonDerivativeBaseBoxShadow="0px 0px 4px rgba(0, 0, 0, 0.25)"
-          buttonDerivativeBaseOverflow="unset"
-          buttonDerivativeBaseJustifyContent="center"
-          buttonBackgroundColor="#fff"
-          labelLineHeight="125%"
-          labelColor="#000"
+          buttonType="light"
           iconXSmall="../iconxsmall21@2x.png"
-          buttonDerivativeBasePosition="absolute"
-          buttonDerivativeBaseWidth="8.83%"
-          buttonDerivativeBaseTop="calc(50% - 290px)"
-          buttonDerivativeBaseRight="19.94%"
-          buttonDerivativeBaseLeft="71.23%"
           iconXSmallDisplay="unset"
         />
       </div>

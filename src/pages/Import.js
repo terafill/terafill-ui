32aa7a78558f19d8f9cd99a67cb7194db31efbef
harshitsaini <harshitsaini15@gmail.com>
2023-03-04 16:09:17 +0530

    Fix Import page

--- src/pages/Import.js
+++ src/pages/Import.js
@@ -3,109 +3,47 @@ import Button from "../components/Button";
 import "./Import.css";
 
 const Import = () => {
+
+  const importList = [
+
+    {"label": "1passoword",
+    "image": "../1passwordicon@2x.png"},
+    {"label": "Lastpass",
+    "image": "../lastpassimage@2x.png"},
+    {"label": "RoboForm",
+    "image": "../roboformimage@2x.png"},
+    {"label": "iCloud passwords",
+    "image": "../apple.svg"},
+    {"label": "Dashlane",
+    "image": "../dashlane.svg"},
+    {"label": "Keepass",
+    "image": "../keepassimage@2x.png"},
+    {"label": "Chrome",
+    "image": "../chrome@2x.png"},
+    {"label": "Firefox",
+    "image": "../firefox-1@2x.png"},
+    {"label": "Keepassxc",
+    "image": "../keepassxcimage@2x.png"},
+    {"label": "Thycotic",
+    "image": "../thycoticimage@2x.png"},
+    {"label": "Other",
+    "image": "../chevronRight.svg"},
+  ];
+
   return (
     <div className="import">
       <Navbar navbarType="app"/>
       <div className="apphomeinner3">
-        <Button
-          label="1password"
-          buttonBackgroundColor="#fff"
-          labelColor="#000"
-          iconXSmall="../1passwordicon@2x.png"
-          iconXSmallDisplay="unset"
-          iconXSmallObjectFit="unset"
-          iconXSmallOverflow="hidden"
-        />
-        <Button
-          label="Lastpass"
-          iconXSmall="../lastpassimage@2x.png"
-          buttonBackgroundColor="#fff"
-          labelColor="#000"
-          iconXSmallDisplay="unset"
-          iconXSmallObjectFit="unset"
-          iconXSmallOverflow="hidden"
-        />
-        <Button
-          label="RoboForm"
-          iconXSmall="../roboformimage@2x.png"
-          buttonBackgroundColor="#fff"
-          labelColor="#000"
-          iconXSmallDisplay="unset"
-          iconXSmallObjectFit="unset"
-          iconXSmallOverflow="hidden"
-        />
-        <Button
-          label="iCloud passwords"
-          buttonBackgroundColor="#fff"
-          labelColor="#000"
-          iconXSmall="../iconxsmall50.svg"
-          iconXSmallDisplay="unset"
-          iconXSmallObjectFit="unset"
-          iconXSmallOverflow="hidden"
-        />
-        <Button
-          label="Dashlane"
-          buttonBackgroundColor="#fff"
-          labelColor="#000"
-          iconXSmall="../dashlane.svg"
-          iconXSmallDisplay="unset"
-          iconXSmallObjectFit="unset"
-          iconXSmallOverflow="hidden"
-        />
-        <Button
-          label="Keepass"
-          buttonBackgroundColor="#fff"
-          labelColor="#000"
-          iconXSmall="../keepassimage@2x.png"
-          iconXSmallDisplay="unset"
-          iconXSmallObjectFit="unset"
-          iconXSmallOverflow="hidden"
-        />
-        <Button
-          label="Chrome"
-          buttonBackgroundColor="#fff"
-          labelColor="#000"
-          iconXSmall="../iconxsmall27@2x.png"
-          iconXSmallDisplay="unset"
-          iconXSmallObjectFit="unset"
-          iconXSmallOverflow="hidden"
-        />
-        <Button
-          label="Firefox"
-          buttonBackgroundColor="#fff"
-          labelColor="#000"
-          iconXSmall="../firefox-1@2x.png"
-          iconXSmallDisplay="unset"
-          iconXSmallObjectFit="unset"
-          iconXSmallOverflow="hidden"
-        />
-        <Button
-          label="Keepassxc"
-          buttonBackgroundColor="#fff"
-          labelColor="#000"
-          iconXSmall="../keepassxcimage@2x.png"
-          iconXSmallDisplay="unset"
-          iconXSmallObjectFit="unset"
-          iconXSmallOverflow="hidden"
-        />
-        <Button
-          label="Thycotic "
-          buttonBackgroundColor="#fff"
-          labelColor="#000"
-          iconXSmall="../thycoticimage@2x.png"
-          iconXSmallDisplay="unset"
-          iconXSmallObjectFit="unset"
-          iconXSmallOverflow="hidden"
-        />
-        <Button
-          label="Other"
-          buttonBackgroundColor="#fff"
-          labelColor="#000"
-          iconXSmall="../vector8.svg"
-          iconXSmallDisplay="unset"
-          iconXSmallObjectFit="unset"
-          iconXSmallOverflow="hidden"
-        />
+      {
+        importList.map(
+          importItem =>
+          <Button
+            label={importItem.label}
+            iconXSmall={importItem.image}
+            buttonType="light"
+            iconXSmallDisplay="unset"
+        />)
+      }
       </div>
     </div>
   );
